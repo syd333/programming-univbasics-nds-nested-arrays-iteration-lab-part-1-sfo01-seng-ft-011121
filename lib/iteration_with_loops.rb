9887d0b6d1475bdcad068193731782e06a4d1f89
sydney miranda <sydmiranda@icloud.com>
2021-01-04 20:13:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
+    if src[row_index][element_index].even?
     element_index += 1
   end
   row_index += 1
